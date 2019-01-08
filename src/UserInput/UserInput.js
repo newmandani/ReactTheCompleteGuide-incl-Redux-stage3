70df2a126e81575373980ec83d49ff4585e98b69
dani <dani@Mac-mini-Paxport.local>
2019-01-08 23:10:40 +0300

    Comments // Stage 1.4

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import './UserInput.css';
 
-const userInput = ({ change, input }) => {
-
-  return (
-    <div className="UserInput">
-      <input type="text" onChange={change} value={input} />
-    </div>
-  );
-}
+const userInput = ({ change, input }) => (
+  <div className="UserInput">
+    <input type="text" onChange={change} value={input} />
+  </div>
+);
 
 export default userInput;
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 import './UserOutput.css';
 
-const userOutput = ({val}) => {
-
-  return (
-    <div className="UserOutput">
-      <p>Hello, men!</p>
-      <p>Are you {val}?</p>
-    </div>
-  );
-}
+const userOutput = ({val}) => (
+  <div className="UserOutput">
+    <p>Hello, men!</p>
+    <p>Are you {val}?</p>
+  </div>
+);
 
 export default userOutput;
